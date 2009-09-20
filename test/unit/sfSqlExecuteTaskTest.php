b12e0ab2bb780b757dd2299664ccf628e3e22f16
Ilya Sabelnikov <fruit.dev@gmail.com>
2009-09-20 21:39:01 +0300

    Examples dir moved to unit folder. Minor updates in the unit test

--- test/unit/sfSqlExecuteTaskTest.php
+++ test/unit/sfSqlExecuteTaskTest.php
@@ -11,6 +11,8 @@
 require_once realpath(dirname(__FILE__) . '/../../../../test/bootstrap/unit.php');
 require_once "{$configuration->getRootDir()}/plugins/sfSQLToolsPlugin/lib/task/sfSqlExecuteTask.class.php";
 
+$sqlFolder = realpath(dirname(__FILE__) . '/../data');
+
 $t = new lime_test(14, new lime_output_color());
 
 $taskCommands = array(
@@ -18,22 +20,22 @@ $taskCommands = array(
   array('args' => array(), 'options' => array('--ff="1"'), 'return' => false, 'incorrect' => true),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--dir="plugins/sfSQLToolsPlugin/data/sql/example-tasks-0"'),
+    'options' => array('--env="devmysql"', '--dir="' . $sqlFolder . '/example-tasks-0"'),
     'return' => true,
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--dir="plugins/sfSQLToolsPlugin/data/sql/example-tasks-0"', '--delimiter="@"'),
+    'options' => array('--env="devmysql"', '--dir="' . $sqlFolder . '/example-tasks-0"', '--delimiter="@"'),
     'return' => false, # files are delimited by ~
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--dir="plugins/sfSQLToolsPlugin/data/sql"'),
+    'options' => array('--env="devmysql"', '--dir="' . $sqlFolder . '"'),
     'return' => false, # folder is empty
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--dir="plugins/sfSQLToolsPlugin/data/sql"', '--dir-depth="1"'),
+    'options' => array('--env="devmysql"', '--dir="' . $sqlFolder . '"', '--dir-depth="1"'),
     'return' => true, # folder has subfolders with sql files
   ),
   array(
@@ -48,29 +50,29 @@ $taskCommands = array(
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--dir="plugins/sfSQLToolsPlugin/data/sql"', '--file="10-create-tables.sql"'),
-    'return' => false, # directory does not exists
+    'options' => array('--env="devmysql"', '--dir="' . $sqlFolder . '"', '--file="10-create-tables.sql"'),
+    'return' => false, # directory has no *.sql files (--dir-depth=0)
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--dir="plugins/sfSQLToolsPlugin/data/sql/example-tasks-0"', '--file="10-create-tables.sql"'),
+    'options' => array('--env="devmysql"', '--dir="' . $sqlFolder . '/example-tasks-0"', '--file="10-create-tables.sql"'),
     'return' => true,
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--file="plugins/sfSQLToolsPlugin/data/sql/10-create-tables.sql"'),
+    'options' => array('--env="devmysql"', '--file="' . $sqlFolder . '/10-create-tables.sql"'),
     'return' => false, # file does not exists
   ),
   array(
     'args' => array(),
-    'options' => array('--env="devmysql"', '--file="plugins/sfSQLToolsPlugin/data/sql/example-tasks-0/10-create-tables.sql"'),
+    'options' => array('--env="devmysql"', '--file="' . $sqlFolder . '/example-tasks-0/10-create-tables.sql"'),
     'return' => true,
   ),
   array(
     'args' => array(),
     'options' => array(
       '--env="devmysql"',
-      '--dir="plugins/sfSQLToolsPlugin/data/sql/example-tasks-0"',
+      '--dir="' . $sqlFolder . '/example-tasks-0"',
       '--exclude="20-inserts.sql, 30-procedures.sql"'
     ),
     'return' => true,
@@ -79,7 +81,7 @@ $taskCommands = array(
     'args' => array(),
     'options' => array(
       '--env="devmysql"',
-      '--dir="plugins/sfSQLToolsPlugin/data/sql/example-tasks-1"',
+      '--dir="' . $sqlFolder . '/example-tasks-1"',
       '--delimiter="/*sql*/"'
     ),
     'return' => true,
